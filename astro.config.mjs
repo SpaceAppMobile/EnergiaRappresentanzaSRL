db559e0127663d05d09c1d2f854ee3bab1cb75b9
Kayo <kayodionizio@gmail.com>
2024-06-18 01:54:01 +0200

    sanity Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import svelte from "@astrojs/svelte";
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), svelte()]
+  integrations: [
+    tailwind(),
+    react(),
+    svelte(),
+    sanity({
+      projectId: "xzyslyix",
+      dataset: "clienti",
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+      // Access the Studio on your.url/admin
+      studioBasePath: "/admin",
+    }),
+  ],
 });
